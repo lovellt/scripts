8afc74720a6ddae236991dad0ec74aa12e32ad9d
lovellt <todd.lovell@hhcc.com>
2015-04-21 15:07:22 -0400

    Position change
    Position change script

---
+++ Positionchange.ps1
@@ -0,0 +1,43 @@
+import-module ActiveDirectory
+#do
+#{
+    $userFName = read-host "Enter first name or Q TO QUIT" 
+    $userLName = read-host "Enter last name or Q TO QUIT"
+    $userName = $userFNAme + " " + $userLName
+    $user = Get-ADUser -filter {name -eq $userName}
+    
+    if (!$user)
+        {
+            write-host "No user found with name" $userName
+        }
+        else
+        {
+            write-host "-----------------------------------"
+            write-host "Current Profile"
+            write-host "-----------------------------------"
+            Get-ADUser $user -Properties * | select-object Name,Title,Department,Company,Office,Description
+            $userTitle = (Get-ADUser $user -Properties *).Title
+            $userTitleNew = read-host ("New Title[" + $userTitle + "]")
+            $userDept = (Get-ADUser $user -Properties *).Department
+            $userDeptNew = read-host ("New Department[" + $userDept + "]")
+            if($userTitleNew -ne "")
+            {
+              $userTitle = $userTitleNew
+            }
+            if($userDeptNew -ne "")
+            {
+              $userDept = $userDeptNew
+            }
+            $userDesc = ($userTitle + " - " + $userDept)
+#            write-host $userTitle
+#            write-host $userDept
+#            write-host $userDesc
+             write-host "-----------------------------------"
+             write-host "updated profile"
+             write-host "-----------------------------------"
+            set-aduser $user -Title $userTitle -Department $userDept -Description $userDesc
+            Get-ADUser $user -Properties * | select-object Name,Title,Department,Company,Office,Description
+         }
+         
+#}while (($userFName -ne "Q") -or ($userLName -ne "Q"))
+
